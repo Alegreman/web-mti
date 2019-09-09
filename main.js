7d36681f8d84a331af5ea40b1e87dc4f3d91f7f1
Jorge Alegria <jobaalegriaji@ittepic.edu.mx>
2019-09-08 22:20:01 -0600

    dddd

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const nav = document.querySelector('.navbar');
+const logo = document.querySelector('.logo');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
@@ -8,6 +10,21 @@ hamburger.addEventListener('click', () => {
     link.classList.toggle('fade');
   });
 });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById('navbar').style.padding = '30px 10px';
+    document.getElementById('logo').style.fontSize = '25px';
+  } else {
+    document.getElementById('navbar').style.padding = '80px 10px';
+    document.getElementById('logo').style.fontSize = '35px';
+  }
+}
 
 /* 
 
